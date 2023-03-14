26af883d87936559f2394c178a81ce611fa2bf10
Impravin9 <127767003+Impravin9@users.noreply.github.com>
2023-03-14 19:59:21 +0530

    Add files via upload

---
+++ LearnJpaAndHibernateApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.springboot.learnjpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@SpringBootApplication
+@EntityScan("com.in28minutes.springboot.learnjpaandhibernate")
+public class LearnJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+	}
+	
+	
+}
